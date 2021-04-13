5e34c9c7aecba16ba746588b17b261a09b50af15
Hicky1025 <plane1025.ict@gmail.com>
2021-04-13 12:57:13 +0900

    master

--- container_setup.sh
+++ container_setup.sh
@@ -2,4 +2,4 @@
 
 read -e -p "plz insert path : " container_path
 mkdir -p $container_path
-cp /Users/kouki/devs/container/container* $container_path
+cp /Users/kouki/devs/container/container.* $container_path
